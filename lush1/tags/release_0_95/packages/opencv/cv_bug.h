b7ff03d2221ba1ffc1aabde24dcb56cfbf76f347
(no author) <(no author)>
2002-11-01 22:02:25 +0000

    This commit was manufactured by cvs2svn to create tag 'release_0_95'.
    git-svn-id: svn://svn.code.sf.net/p/lush/code@1470 0babfeb2-de4b-461b-a2bf-1ab085750f0d

---
+++ lush1/tags/release_0_95/packages/opencv/cv_bug.h
@@ -0,0 +1,52 @@
+typedef struct _CvContourInfo
+{
+    struct _CvContourInfo *next;        /* next contour with the same mark value */
+    struct _CvContourInfo *parent;      /* information about parent contour */
+    CvSeq *contour;             /* corresponding contour (may be 0, if rejected) */
+    CvRect rect;                /* bounding rectangle */
+    CvPoint origin;             /* origin point (where the contour was traced from) */
+    int is_hole;                /* hole flag */
+} _CvContourInfo;
+
+
+/*
+  Structure that is used for sequental retrieving contours from the image.
+  It supports both hierarchical and plane variants of Suzuki algorithm.
+*/
+typedef struct _CvContourScanner
+{
+    CvMemStorage *storage1;     /* contains fetched contours */
+    CvMemStorage *storage2;     /* contains approximated contours
+                                   (!=storage1 if approx_method2 != approx_method1) */
+    CvMemStorage *cinfo_storage;        /* contains _CvContourInfo nodes */
+    CvSet *cinfo_set;           /* set of _CvContourInfo nodes */
+    CvMemStoragePos initial_pos;        /* starting storage pos */
+    CvMemStoragePos backup_pos; /* beginning of the latest approx. contour */
+    CvMemStoragePos backup_pos2;        /* ending of the latest approx. contour */
+    char *img0;                 /* image origin */
+    char *img;                  /* current image row */
+    int img_step;               /* image step */
+    CvSize img_size;            /* ROI size */
+    CvPoint pt;                 /* current scanner position */
+    CvPoint lnbd;               /* position of the last met contour */
+    int nbd;                    /* current mark val */
+    _CvContourInfo *l_cinfo;    /* information about latest approx. contour */
+    _CvContourInfo cinfo_temp;  /* temporary var which is used in simple modes */
+    _CvContourInfo frame_info;  /* information about frame */
+    CvSeq frame;                /* frame itself */
+    CvChainApproxMethod approx_method1; /* approx method when tracing */
+    CvChainApproxMethod approx_method2; /* final approx method */
+    int mode;                   /* contour scanning mode:
+                                   0 - external only
+                                   1 - all the contours w/o any hierarchy
+                                   2 - connected components (i.e. two-level structure -
+                                   external contours and holes) */
+    int subst_flag;
+    int seq_type1;              /* type of fetched contours */
+    int header_size1;           /* hdr size of fetched contours */
+    int elem_size1;             /* elem size of fetched contours */
+    int seq_type2;              /*                                       */
+    int header_size2;           /*        the same for approx. contours  */
+    int elem_size2;             /*                                       */
+    _CvContourInfo *cinfo_table[126];
+} _CvContourScanner;
