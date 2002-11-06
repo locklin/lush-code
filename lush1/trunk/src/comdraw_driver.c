0bd7fd8bd9e0b567af5023ade66fd629500dc578
leonb <leonb>
2002-11-06 21:17:53 +0000

    More preparation for comdraw_driver.
    git-svn-id: svn://svn.code.sf.net/p/lush/code@1489 0babfeb2-de4b-461b-a2bf-1ab085750f0d

--- lush1/trunk/src/comdraw_driver.c
+++ lush1/trunk/src/comdraw_driver.c
@@ -26,9 +26,32 @@
 /* Secil Ugurel, Aug 2002   */
 
 
+#include "header.h"
+#include "graphics.h"
+#include <limits.h>
+#include <errno.h>
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+#ifdef TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# ifdef HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
+/**************************************************************/
+
 
 /* Empty for now */
 
+
+
+/**************************************************************/
 
 void 
 init_comdraw_driver(void)
