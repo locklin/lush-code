b3d525e410eb1dab0b56cab0944c101f15958802
ysulsky <ysulsky>
2007-04-02 01:27:00 +0000

    multi-line editing
    git-svn-id: svn://svn.code.sf.net/p/lush/code@2642 0babfeb2-de4b-461b-a2bf-1ab085750f0d

--- lush1/trunk/etc/lush.el
+++ lush1/trunk/etc/lush.el
@@ -24,7 +24,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; $Id: lush.el,v 1.9 2007-04-01 09:03:25 ysulsky Exp $
+;;; $Id: lush.el,v 1.10 2007-04-02 01:27:00 ysulsky Exp $
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; this file contains useful definitions for emacs
@@ -38,20 +38,28 @@
 (defvar *lush-symbols* ())
 (defvar *lush-symbols-stale?* t)
 
+(defun lush-proc ()
+  (condition-case err
+      (inferior-lisp-proc)
+    (error ())))
+
 ;; call this with M-X lush
 (defun lush ()
   "starts Lush"
   (interactive)
   (inferior-lisp "lush")
   (setq comint-prompt-regexp "^?")
+  (setq comint-get-old-input #'lush-get-incomplete-input)
   (if cache-symbols
       (progn
         ;; override comint-send-string to check for definitions/file loads
         ;; entered by hand in the repl or through the lisp-load-file command
         (defvar prev-comint-send-string (symbol-function 'comint-send-string))
         (defun comint-send-string (proc inp)
-          (if (string-match "load\\|\\^L\\|def\\|dmd\\|set\\|(d[efmz][\\t\\n ]" inp)
-              (setq *lush-symbols-stale?* t))
+          (let ((stale-re "load\\|\\^L\\|def\\|dmd\\|set\\|(d[efmz][\\t\\n ]"))
+            (if (and (eq proc (lush-proc))
+                     (string-match stale-re inp))
+                (setq *lush-symbols-stale?* t)))
           (funcall prev-comint-send-string proc inp))
 
         ;; override comint-send-region to mark the symbol cache stale
@@ -60,42 +68,88 @@
         (defun comint-send-region (proc start end)
           (setq *lush-symbols-stale?* t)
           (funcall prev-comint-send-region proc start end))))
-
+    
   (set (make-local-variable 'pcomplete-parse-arguments-function)
        'lush-parse-arguments)
 
+  (local-set-key "\r" 'lush-filter-input)
+  (local-set-key "\n" 'lush-filter-input)
   (local-set-key "\t" 'pcomplete))
 
 (global-set-key "\C-xg" 'goto-line)
+
+
+(defun lush-avail? (mark)
+  (let ((prompt (save-excursion
+                     (search-backward-regexp "^?" nil t))))
+    (and prompt (= mark (+ prompt 2)))))
+
+(defun lush-filter-input ()
+  (interactive)
+  (let ((proc (lush-proc)))
+    (if (and proc (eq (current-buffer) (process-buffer proc)))
+        (progn
+          (insert "\n")
+          (let ((cur (point))
+                (end (point-max))
+                (mark (process-mark proc)))
+            (if (lush-avail? mark)
+                ;; we're at the repl, not in debug or other i/o
+                (let ((status (with-syntax-table lisp-mode-syntax-table
+                                (parse-partial-sexp mark end))))
+                  (if (and (= (car status) 0)   ; balanced parens
+                           (not (elt status 3)) ; inside string
+                           (not (elt status 4)) ; inside comment
+                           (<= (point) cur))    ; not inside the expression
+                      (let ((expr (buffer-substring
+                                   (elt status 2) (- (point) 1))))
+                        (comint-send-string proc (concat expr "\n"))
+                        (comint-add-to-input-history expr)
+                        (command-execute 'comint-set-process-mark))
+                    (goto-char cur)))
+              ;; just send the input to lush
+              (let ((expr (buffer-substring-no-properties mark (- end 1))))
+                (comint-send-string proc (concat expr "\n"))
+                (comint-add-to-input-history expr)
+                (command-execute 'comint-set-process-mark))))))))
 
 (defun filter (p lst)
   (let ((ret ()))
     (mapc (lambda (x) (if (funcall p x) (setq ret (cons x ret)))) lst)
     (nreverse ret)))
 
-;; be careful; calling this after entering an unfinished expression
-;; will cause this to hang
+;; be careful; calling this when lush isn't available will cause this to hang
 (defun lush-symbols ()
-  (car (read-from-string
-        (car (comint-redirect-results-list-from-process
-              (inferior-lisp-proc)
-              "(symblist)"
-              "(.*)" 0)))))
+  (let ((proc (lush-proc)))
+    (if proc
+        (car (read-from-string
+              (car (comint-redirect-results-list-from-process
+                    proc "(symblist)" "(.*)" 0))))
+      ())))
 
 (defun lush-complete (sym)
   (let* ((sym-name (downcase (if (stringp sym) sym (symbol-name sym))))
-         (minl (length sym-name)))
+         (minl (length sym-name))
+         (proc (lush-proc)))
     ;; if we have the prompt and the symbols are stale, update *lush-symbols*
     (if (and *lush-symbols-stale?*
-             (> (save-excursion (search-backward-regexp "^?" nil t)) 
-                (save-excursion (search-backward-regexp "\n" nil t))))
-        (progn (setq *lush-symbols* (lush-symbols))
-               (if cache-symbols
-                   (setq *lush-symbols-stale?* ()))))
+             proc
+             (eq (current-buffer) (process-buffer proc))
+             (lush-avail? (process-mark proc)))
+        (progn
+          (setq *lush-symbols* (lush-symbols))
+          (if cache-symbols
+              (setq *lush-symbols-stale?* ()))))
     (filter (lambda (s)
               (and (>= (length s) minl)
                    (string= sym-name (substring s 0 minl))))
             *lush-symbols*)))
+
+(defun lush-get-incomplete-input ()
+  (let ((proc (lush-proc)))
+    (if (and proc (eq (current-buffer) (process-buffer proc)))
+        (buffer-substring (process-mark proc) (point-max))
+      "")))
 
 ;; see http://www.emacswiki.org/cgi-bin/wiki/PcompleteExamples
 (defun lush-parse-arguments ()
