689971214c4e504a2a11008d5cdd670e4dc3a12d
leonb <leonb>
2024-03-14 00:38:13 +0000

    TIME_WITH_SYS_TIME is considered obsolete (and breaks now)
    git-svn-id: svn://svn.code.sf.net/p/lush/code@2837 0babfeb2-de4b-461b-a2bf-1ab085750f0d

--- lush1/trunk/src/date.c
+++ lush1/trunk/src/date.c
@@ -37,16 +37,10 @@
  */
 #include "header.h"
 
-#ifdef TIME_WITH_SYS_TIME
-# include <sys/times.h>
-# include <time.h>
-#else
-# ifdef HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
+#ifdef HAVE_SYS_TIME_H
+# include <sys/time.h>
 #endif
+#include <time.h>
 
 #define MINDATE 0
 #define MAXDATE 6
